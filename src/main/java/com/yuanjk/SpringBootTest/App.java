e93e184f036d489162632cef73cfecb7f8248314
yuanjk <yuanjk@asiainfo-mixdata.com>
2018-04-22 16:51:12 +0800

    new project

---
+++ src/main/java/com/yuanjk/SpringBootTest/App.java
@@ -0,0 +1,13 @@
+package com.yuanjk.SpringBootTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
